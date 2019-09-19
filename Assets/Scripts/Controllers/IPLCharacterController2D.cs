df06bec74684088064dbe3ca538177b27c34c931
MSI\jeang <jeangodecoster@gmail.com>
2019-09-19 17:04:34 +0200

    Semaine 1

---
+++ Assets/Scripts/Controllers/IPLCharacterController2D.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class IPLCharacterController2D: MonoBehaviour
+{
+
+    // add speed control
+    [SerializeField]
+    int speed;
+
+    float horizontalSpeed;
+
+    Transform myTransform;
+    Rigidbody2D myRigidbody;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ReadInput()
+    {
+        horizontalSpeed = Input.GetAxis("Horizontal") * speed;
+    }
+
+    private void Move()
+    {
+        //        transform.Translate(new Vector2(horizontalSpeed, 0f));
+        myRigidbody.velocity = new Vector2(horizontalSpeed, myRigidbody.velocity.y);
+    }
+}
---
+++ Assets/Scripts/HelloWorld/HelloWorld.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    [SerializeField]
+    string warningMessage = "Greta, save us!";
+
+    int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+        Debug.LogWarning(warningMessage);
+    }
+
+    private void OnEnable()
+    {
+        print("I'm on");
+    }
+    private void OnDisable()
+    {
+        print("I'm off");
+    }
+    private void OnDestroy()
+    {
+        print("QQ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            print("Jump");
+        print(Input.GetAxisRaw("Horizontal"));
+    }
+}
