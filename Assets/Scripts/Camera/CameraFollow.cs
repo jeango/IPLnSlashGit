c7cd3c362c269b18ce18d91dcb6f084c0cd237e1
MSI\jeang <jeangodecoster@gmail.com>
2019-09-23 17:49:43 +0200

    Fin Cours2

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    Transform myTransform;
+
+    Vector3 originalOffset;
+
+    private void Start()
+    {
+        myTransform = transform;
+        originalOffset = myTransform.position - target.position;
+    }
+
+    private void Update()
+    {
+        myTransform.position = target.position + originalOffset;
+    }
+}
--- Assets/Scripts/Controllers/IPLCharacterController2D.cs
+++ Assets/Scripts/Controllers/IPLCharacterController2D.cs
@@ -9,8 +9,20 @@ public class IPLCharacterController2D: MonoBehaviour
     // add speed control
     [SerializeField]
     int speed;
+    [SerializeField]
+    int jumpForce;
+    [SerializeField]
+    Transform groundCheckA;
+    [SerializeField]
+    Transform groundCheckB;
+    [SerializeField]
+    LayerMask groundLayers;
 
     float horizontalSpeed;
+    float jumpSpeed;
+
+    bool isGrounded;
+    bool isFlipped;
 
     Transform myTransform;
     Rigidbody2D myRigidbody;
@@ -27,6 +39,8 @@ public class IPLCharacterController2D: MonoBehaviour
     void Update()
     {
         ReadInput();
+        CheckGround();
+        CheckFlip();
     }
 
     private void FixedUpdate()
@@ -34,14 +48,35 @@ public class IPLCharacterController2D: MonoBehaviour
         Move();
     }
 
+    void CheckFlip()
+    {
+        if ((horizontalSpeed > 0.001f && isFlipped) || (horizontalSpeed < -0.001f && !isFlipped))
+            UpdateFlip();
+    }
+
+    void UpdateFlip()
+    {
+        isFlipped = !isFlipped;
+        float rotationAngle = isFlipped ? 180f : -180f;
+        myTransform.Rotate(new Vector3(0f, rotationAngle, 0f), Space.Self);
+    }
+
     void ReadInput()
     {
         horizontalSpeed = Input.GetAxis("Horizontal") * speed;
+        jumpSpeed = Input.GetAxisRaw("Jump") * jumpForce;
+    }
+
+    void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapArea(groundCheckA.position, groundCheckB.position, groundLayers);
     }
 
     private void Move()
     {
         //        transform.Translate(new Vector2(horizontalSpeed, 0f));
-        myRigidbody.velocity = new Vector2(horizontalSpeed, myRigidbody.velocity.y);
+        bool shouldJump = (jumpSpeed > 0f) && isGrounded;
+        float yVelocity = shouldJump ? jumpSpeed : myRigidbody.velocity.y;
+        myRigidbody.velocity = new Vector2(horizontalSpeed, yVelocity);
     }
 }
